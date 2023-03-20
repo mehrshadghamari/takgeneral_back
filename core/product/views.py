9b6da5795985d364c5c8c17b11f27c27b95475cf
ghamarimehrshad1 <mehrshad2014ghamari@gmail.com>
2023-03-20 20:01:54 +0330

    adding id product api

--- core/product/views.py
+++ core/product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -7,7 +7,8 @@ from .serializers import HomePompDetailSerializer,ProductIDSerializer
 
 class HomePompDetail(APIView):
     def get(self,request,id):
-        pomp_instance=Product.objects.filter(id=id).first()
+        # pomp_instance=Product.objects.filter(id=id).first()
+        pomp_instance=get_object_or_404(Product,id=id)
         serilizer=HomePompDetailSerializer(pomp_instance,context={"request": request})
         return Response(serilizer.data,status=status.HTTP_200_OK)
     
