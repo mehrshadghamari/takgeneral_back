714e481d4d9b38335a30f8f9dd30589caf227b0f
ghamarimehrshad1 <mehrshad2014ghamari@gmail.com>
2023-03-20 19:39:27 +0330

    adding id product api

--- core/product/views.py
+++ core/product/views.py
@@ -14,7 +14,7 @@ class HomePompDetail(APIView):
 
 
 class ProductID(APIView):
-    def get(self,request,id):
+    def get(self,request):
         ids=Product.objects.all().values_list('id')[:30]
         srz=ProductIDSerializer(ids)
         return Response(srz.data,status=status.HTTP_200_OK)
