a93d4c35127d9660eace83354681065be0d861fd
ghamarimehrshad1 <mehrshad2014ghamari@gmail.com>
2023-03-20 17:33:22 +0330

    some change

--- core/product/models.py
+++ core/product/models.py
@@ -14,7 +14,7 @@ class ProductImage(models.Model):
 
 # category many to many
 class ProductCategory(models.Model):
-    neme=models.CharField(max_length=255)
+    name=models.CharField(max_length=255)
 
     def __str__(self):
         return self.neme
