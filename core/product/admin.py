c7c87342ef48fc313110e7000a16c6df3b427e83
mehrshad <ghamarimehrshad@gmail.com>
2023-07-19 14:42:35 +0330

    handel specification admin

--- core/product/admin.py
+++ core/product/admin.py
@@ -1,12 +1,10 @@
 from django import forms
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
 from extention.admin import ContentInline
 from mptt.admin import MPTTModelAdmin
 from nested_inline.admin import NestedModelAdmin
-from nested_inline.admin import NestedTabularInline
 
-# from .models import ProductCategory
-# from .models import Attribute
 from .models import Category
 from .models import Product
 from .models import ProductBrand
@@ -15,35 +13,12 @@ from .models import ProductSpecification
 from .models import ProductSpecificationValue
 from .models import ProductType
 
-# from .models import TitleAttribute
-
 # Register your models here.
 
-
-# admin.site.register(HomePomp)
-# admin.site.register(Product)
-# admin.site.register(ProductImage)
 admin.site.register(ProductBrand)
-# admin.site.register(ProductCategory)
-# admin.site.register(Attribute)
-# admin.site.register(TitleAttribute)
 
 
-###################release 2 admin
 
-# class ProductAdminForm(forms.ModelForm):
-#     class Meta:
-#         model = Product
-#         fields = '__all__'
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         # Filter the ProductSpecificationValueInline based on the selected ProductType
-#         product_type = self.instance.product_type
-#         if product_type:
-#             self.fields['productspecificationvalue_set'].queryset = ProductSpecificationValue.objects.filter(
-#                 product=self.instance
-#             )
 
 admin.site.register(Category,MPTTModelAdmin)
 
@@ -63,46 +38,62 @@ class ProductImageInline(admin.TabularInline):
 
 
 
+
+
+
 # class ProductSpecificationValueInline(admin.TabularInline):
 #     model = ProductSpecificationValue
 
+#     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+#         if db_field.name == 'specification':
+#             product_id = request.resolver_match.kwargs.get('object_id')
+#             if product_id:
+#                 product = Product.objects.get(pk=product_id)
+#                 kwargs['queryset'] = ProductSpecification.objects.filter(product_type=product.product_type)
+#             else:
+#                 kwargs['queryset'] = ProductSpecification.objects.all()
+#         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     # form = ProductAdminForm
-#     inlines=[
-#         ProductImageInline,
-#         ProductSpecificationValueInline,
-#     ]
+
+
+
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        product_type = cleaned_data.get('product_type')
+        if not product_type:
+            raise forms.ValidationError("A product type must be selected.")
+        return cleaned_data
 
 
 class ProductSpecificationValueInline(admin.TabularInline):
     model = ProductSpecificationValue
 
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        form.base_fields['product_type'].widget.can_add_related = False
-        return form
-
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'product':
-            kwargs['queryset'] = Product.objects.all()
         if db_field.name == 'specification':
-            product_type_id = request.POST.get('product_type', None)
-            if product_type_id:
-                kwargs['queryset'] = ProductSpecification.objects.filter(
-                    id=product_type_id
-                )
+            product_id = request.resolver_match.kwargs.get('object_id')
+            if product_id:
+                product = Product.objects.get(pk=product_id)
+                kwargs['queryset'] = ProductSpecification.objects.filter(product_type=product.product_type)
             else:
-                kwargs['queryset'] = ProductSpecification.objects.all()
+                kwargs['queryset'] = ProductSpecification.objects.none()
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+
+
 
 
 @admin.register(Product)
 class ProductAdmin(NestedModelAdmin):
     # ...
-
+    form = ProductForm
     inlines = [
         ProductImageInline,
         ProductSpecificationValueInline,
